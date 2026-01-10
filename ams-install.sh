56403a61bd1a4fc266abe695771ddf2bef8e856a
Amsilla <86023563+Fairies-Feast@users.noreply.github.com>
2026-01-09 20:54:30 -0600

    Add -y flag to apt install for python3.13-venv

--- ams-install.sh
+++ ams-install.sh
@@ -34,10 +34,9 @@ curl https://amsilla-ams.pages.dev/ams-temp.sh >> ams-temp.sh
 sudo -u "$SUDO_USER" bash ams-temp.sh
 curl https://amsilla-ams.pages.dev/ams-app.py >> ams-app.py
 rm ams-temp.sh
-apt install python3.13-venv
+apt install -y python3.13-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install requests
-read
 clear
 echo "Installation is now complete. To open AMS, type 'ams' into the terminal."
